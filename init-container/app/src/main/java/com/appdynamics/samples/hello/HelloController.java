fbba5e0c2587f06fd0f37c71fbdded42324134f6
Cameron Martin <cameron@cameronmartin.com>
2020-08-18 13:30:45 +1000

    Basic container

---
+++ init-container/app/src/main/java/com/appdynamics/samples/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.appdynamics.samples.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
